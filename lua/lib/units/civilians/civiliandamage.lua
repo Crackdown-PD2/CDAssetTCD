4561dfb0ce07db632768c003de115e3a1814f5b0
offyerrocker <offyerrocker88@gmail.com>
2025-05-25 02:47:55 -0700

    False Idol Basic pt1
    civilians are invulnerable while prone
    (changed from previous implementation: civilians no longer need to be tied to gain invulnerability)
    note: early trade (to restore a down) is not yet implemented and will be added along with the other trade/brain related skills later

--- lua/lib/units/civilians/civiliandamage.lua
+++ lua/lib/units/civilians/civiliandamage.lua
@@ -1,8 +1,10 @@
 local orig_damage_bullet = CivilianDamage.damage_bullet
 function CivilianDamage:damage_bullet(attack_data,...)
-	local brain = self._unit:brain()
-	if brain and brain:is_tied() and managers.player:has_team_category_upgrade("player","civilian_hostage_stationary_invuln") then 
-		return
+	if managers.player:has_team_category_upgrade("player","civilian_hostage_stationary_invuln") then 
+		local brain = self._unit:brain()
+		if brain and brain:is_tied() or self._unit:anim_data().drop then
+			return
+		end
 	end
 	
 	return orig_damage_bullet(self,attack_data,...)
@@ -10,9 +12,11 @@ end
 
 local orig_damage_expl = CivilianDamage.damage_explosion
 function CivilianDamage:damage_explosion(attack_data,...)
-	local brain = self._unit:brain()
-	if brain and brain:is_tied() and managers.player:has_team_category_upgrade("player","civilian_hostage_stationary_invuln") then 
-		return
+	if managers.player:has_team_category_upgrade("player","civilian_hostage_stationary_invuln") then 
+		local brain = self._unit:brain()
+		if brain and brain:is_tied() or self._unit:anim_data().drop then
+			return
+		end
 	end
 	
 	return orig_damage_expl(self,attack_data,...)
@@ -20,9 +24,11 @@ end
 
 local orig_damage_fire = CivilianDamage.damage_fire
 function CivilianDamage:damage_fire(attack_data,...)
-	local brain = self._unit:brain()
-	if brain and brain:is_tied() and managers.player:has_team_category_upgrade("player","civilian_hostage_stationary_invuln") then 
-		return
+	if managers.player:has_team_category_upgrade("player","civilian_hostage_stationary_invuln") then 
+		local brain = self._unit:brain()
+		if brain and brain:is_tied() or self._unit:anim_data().drop then
+			return
+		end
 	end
 	
 	return orig_damage_fire(self,attack_data,...)
@@ -30,9 +36,11 @@ end
 
 local orig_damage_melee = CivilianDamage.damage_melee
 function CivilianDamage:damage_melee(attack_data,...)
-	local brain = self._unit:brain()
-	if brain and brain:is_tied() and managers.player:has_team_category_upgrade("player","civilian_hostage_stationary_invuln") then 
-		return
+	if managers.player:has_team_category_upgrade("player","civilian_hostage_stationary_invuln") then 
+		local brain = self._unit:brain()
+		if brain and brain:is_tied() or self._unit:anim_data().drop then
+			return
+		end
 	end
 	
 	return orig_damage_melee(self,attack_data,...)
@@ -40,9 +48,11 @@ end
 
 local orig_damage_tase = CivilianDamage.damage_tase
 function CivilianDamage:damage_tase(attack_data,...)
-	local brain = self._unit:brain()
-	if brain and brain:is_tied() and managers.player:has_team_category_upgrade("player","civilian_hostage_stationary_invuln") then 
-		return
+	if managers.player:has_team_category_upgrade("player","civilian_hostage_stationary_invuln") then 
+		local brain = self._unit:brain()
+		if brain and brain:is_tied() or self._unit:anim_data().drop then
+			return
+		end
 	end
 	
 	return orig_damage_tase(self,attack_data,...)
